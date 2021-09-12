a59984faba9c0ff172326280d10ae92b90995e1c
Daniel Dorndorf <dorndorf@featdd.de>
2021-09-12 15:57:12 +0200

    [TASK] Update copyright year

--- Classes/Controller/ConsentController.php
+++ Classes/Controller/ConsentController.php
@@ -9,7 +9,7 @@ namespace Mindshape\MindshapeCookieConsent\Controller;
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- *  (c) 2020 Daniel Dorndorf <dorndorf@mindshape.de>, mindshape GmbH
+ *  (c) 2021 Daniel Dorndorf <dorndorf@mindshape.de>, mindshape GmbH
  *
  ***/
 
