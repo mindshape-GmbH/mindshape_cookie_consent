32cb93484680261c14443abf49a31b3e6f9bf3d0
Daniel Dorndorf <dorndorf@mindshape.de>
2021-12-01 16:06:28 +0100

    [TASK] Adjust emconf dependency versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.2-11.4.99',
-            'php' => '7.2.0-7.4.99',
+            'typo3' => '10.4.2-11.5.99',
+            'php' => '7.2.0-8.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
