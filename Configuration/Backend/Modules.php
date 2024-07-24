353790e0f76ccb8b073cdda215a9868132badb90
Daniel Dorndorf <dorndorf@mindshape.de>
2024-07-24 16:22:45 +0200

    [TASK] Remove deprecated access key from module registration

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -11,7 +11,7 @@ return [
     ],
     SettingsUtility::EXTENSION_NAME . '_statistics' => [
         'parent' => SettingsUtility::EXTENSION_NAME,
-        'access' => 'user,group',
+        'access' => 'user',
         'iconIdentifier' => 'module-' . SettingsUtility::EXTENSION_NAME . '-statistic',
         'labels' => 'LLL:EXT:' . SettingsUtility::EXTENSION_KEY . '/Resources/Private/Language/module_statistic_locallang.xlf',
         'extensionName' => SettingsUtility::EXTENSION_NAME,
